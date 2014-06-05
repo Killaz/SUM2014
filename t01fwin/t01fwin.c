2720f3f9a56ba510d8398a45dfdbc12375b33dea
unknown <SPR04@R111-25.(none)>
2014-06-05 18:46:11 +0400

    Added Double Buff. Warnings excluded

--- t01fwin/t01fwin.c
+++ t01fwin/t01fwin.c
@@ -5,20 +5,34 @@
 #include <windows.h>
 
 #define WND_CLASS_NAME "My window class"
+#undef _WIN32_WINNT
 #define _WIN32_WINNT 0x0500
 
 #define crand (rand() % 255)
 #define r0 ((int)((float) rand() / RAND_MAX))
 #define sqr(a) ((a) * (a))
+
+#ifndef min
 #define min(a, b) ((a) < (b) ? (a) : (b))
+#endif
+
+#ifndef max
 #define max(a, b) ((a) > (b) ? (a) : (b))
+#endif
 
 
 #ifdef RANDOMIZE
 #include <time.h>
 #endif
 
+#pragma warning(disable: 4244)
+
+typedef char bool;
+
 LRESULT CALLBACK WindowFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam);
+
+INT W, H;
+
 
 INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, CHAR *CmdLine, INT ShowCmd )
 {
@@ -64,47 +78,58 @@ INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, CHAR *CmdLine,
   return msg.wParam;
 }
 
-void DrawEye( HWND hWnd, POINT e, POINT pt, INT R, INT r )
+void DrawEye( HWND hWnd, HBITMAP hBm, POINT e, POINT pt, INT R, INT r, bool k )
 {
   POINT tmp;
-  HDC hDC;
+  HDC hDC, hMemDC;
+  PAINTSTRUCT ps;
 
-  hDC = GetDC(hWnd);
-  SelectObject(hDC, GetStockObject(DC_BRUSH));
-  SetDCBrushColor(hDC, RGB(11, 40, 142));
-  if (sqr(pt.x - e.x) + sqr(pt.y - e.y) < sqr(R - r)) /* LEFT EYE*/
-    Ellipse(hDC, pt.x - r, pt.y - r, pt.x + r, pt.y + r);
+  /*hDC = GetDC(hWnd);*/
+  hDC = BeginPaint(hWnd, &ps);
+  hMemDC = CreateCompatibleDC(hDC);
+
+  SelectObject(hMemDC, hBm);
+
+  if (k)
+  {
+    SelectObject(hMemDC, GetStockObject(DC_BRUSH));
+    SetDCBrushColor(hMemDC, RGB(255, 255, 255));
+    Rectangle(hMemDC, 0, 0, W, H);
+  }
+  
+  SelectObject(hMemDC, GetStockObject(DC_PEN));
+  SetDCPenColor(hMemDC, RGB(0, 0, 0));
+  Ellipse(hMemDC, e.x - R, e.y - R, e.x + R, e.y + R);
+
+  SelectObject(hMemDC, GetStockObject(DC_BRUSH));
+  SetDCBrushColor(hMemDC, RGB(11, 40, 142));
+  if (sqr(pt.x - e.x) + sqr(pt.y - e.y) < sqr(R - r))
+    Ellipse(hMemDC, pt.x - r, pt.y - r, pt.x + r, pt.y + r);
   else
   {
     tmp.x = e.x + ((pt.x - e.x) / sqrt(sqr(pt.x - e.x) + sqr(pt.y - e.y))) * (R - r);
     tmp.y = e.y + ((pt.y - e.y) / sqrt(sqr(pt.x - e.x) + sqr(pt.y - e.y))) * (R - r);
-    Ellipse(hDC, tmp.x - r, tmp.y - r, tmp.x + r, tmp.y + r);
+    Ellipse(hMemDC, tmp.x - r, tmp.y - r, tmp.x + r, tmp.y + r);
   }
-  ReleaseDC(hWnd, hDC);
+
+  BitBlt(hDC, 0, 0, W, H, hMemDC, 0, 0, SRCCOPY);
+  DeleteDC(hMemDC);
+  EndPaint(hWnd, &ps);
+
+  /*ReleaseDC(hWnd, hDC);/**/
 }
 
 LRESULT CALLBACK WindowFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
 {
-  char output[150];
   INT R, r;
+  static HBITMAP hBm;
   HDC hDC;
-  PAINTSTRUCT ps;
-  RECT rc;
   POINT pt, le, re;
 
-  GetClientRect(hWnd, &rc);
-  le.x = 0.25 * rc.right, le.y = 0.5 * rc.bottom;
-  re.x = 0.75 * rc.right, re.y = 0.5 * rc.bottom;
-  //R = min(rc.right, min(rc.bottom, abs(le.x - re.x))) / 3;
-  R = min(rc.bottom / 2, (re.x - le.x) / 2.2);
+  le.x = 0.25 * W, le.y = 0.5 * H;
+  re.x = 0.75 * W, re.y = 0.5 * H;
+  R = min(H / 2, (re.x - le.x) / 2.2);
   r = R / 5;
-
-  hDC = BeginPaint(hWnd, &ps);
-  SelectObject(hDC, GetStockObject(DC_PEN));
-  SetDCPenColor(hDC, RGB(0, 0, 0));
-  Ellipse(hDC, le.x - R, le.y - R, le.x + R, le.y + R);
-  Ellipse(hDC, re.x - R, re.y - R, re.x + R, re.y + R);
-  EndPaint(hWnd, &ps);
 
   switch (Msg)
   {
@@ -112,22 +137,38 @@ LRESULT CALLBACK WindowFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
     SetTimer(hWnd, 4, 10, NULL);
     break;
   case WM_LBUTTONDOWN:
-    InvalidateRect(hWnd, NULL, 1);
+    InvalidateRect(hWnd, NULL, 0);
     break;
+  case WM_ERASEBKGND:
+    return 1;
+  case WM_CHAR:
+    if ((CHAR) wParam == 27)
+      DestroyWindow(hWnd);
+    break;
+  case WM_SIZE:
+    W = LOWORD(lParam);
+    H = HIWORD(lParam);
+    hDC = GetDC(hWnd);
+    if (hBm != NULL)
+      DeleteObject(hBm);
+    hBm = CreateCompatibleBitmap(hDC, W, H);
+    ReleaseDC(hWnd, hDC);
+    return 0;
   case WM_TIMER:
-    InvalidateRect(hWnd, NULL, 1);
+    InvalidateRect(hWnd, NULL, 0);
     GetCursorPos(&pt);
     ScreenToClient(hWnd, &pt);
     
-    DrawEye(hWnd, le, pt, R, r);
-    DrawEye(hWnd, re, pt, R, r);
+    DrawEye(hWnd, hBm, re, pt, R, r, 0);
+    DrawEye(hWnd, hBm, le, pt, R, r, 1);
 
     /*tmp.x = rc.right / 2, tmp.y = rc.bottom / 2;
     DrawEye(hWnd, tmp, pt, R, r);*/
 
-    Sleep(31);
+    /*Sleep(31);*/
     break;
   case WM_DESTROY:
+    DeleteObject(hBm);
     PostQuitMessage(0);
     KillTimer(hWnd, 4);
     return 0;
