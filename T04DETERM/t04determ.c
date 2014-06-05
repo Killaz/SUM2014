fe6ad2a86f906eceb7b189d16345a563542ac8b8
unknown <SPR04@R111-25.(none)>
2014-06-05 18:47:39 +0400

    First and last version of Determinant finder

--- T04DETERM/t04determ.c
+++ T04DETERM/t04determ.c
@@ -1,128 +1,93 @@
 #include <stdio.h>
 #include <time.h>
-
-#define N 10001
+#include <math.h>
 
 #ifdef DEBUG
-#define lol() printf("Lol\n");
+#include <stdlib.h>
+#endif
+
+#define N 101
+#define FNAME "Matrix.in"
+
+#ifdef DEBUG
+#define lol() fprintf(stderr, "Lol\n");
 #else
-#define lol() ;
+#define lol()  
 #endif
 
 typedef char bool;
 
-int pr[N];
-double M[N][N], tmp, Ans = 0;
+double m[N][N];
 
-int MergeSort( int *a, int l, int r );
-
-void swap( int *a, int *b )
+/*void PrintArr( int n )
 {
-	int tmp = *a;
+	int i, j;
+	for (i = 0; i < n; i++, printf("\n"))
+		for (j = 0; j < n; j++)
+			fprintf(stderr, "%.3lf ", m[i][j]);
+	fprintf(stderr, "\n");
+}*/
+
+void swap( double *a, double *b )
+{
+	double tmp = *a;
 	*a = *b;
 	*b = tmp;
 }
 
-void ReverseArr( int *m, int a, int b )
-{
-	int i;
-	for (i = a; i <= a + (b - a) / 2; i++)
-		swap(&m[i], &m[b - (i - a)]);
-}
-
-void CopyArr( int *a, int *b, int n)
+void SwapW( int n, int a, int b ) /* | | */
 {
 	int i;
 	for (i = 0; i < n; i++)
-		b[i] = a[i];
+		swap(&m[i][a], &m[i][b]);
 }
 
-void PrintArr( int *a, int n, int num, int inv )
-{
+void SwapH( int n, int a, int b ) /* --- */
+{                                 /* --- */
 	int i;
-	printf("%5d: ", num);
 	for (i = 0; i < n; i++)
-		printf("%d ", a[i]);
-	printf("  -  %2d invertions\n", inv);	
+		swap(&m[a][i], &m[b][i]);
 }
 
-void PlusDet( int *p, int n )
+void Minus( int n, int a, int b )
 {
 	int i;
-	CopyArr(p, pr, n);
-	tmp = 1;
-   	for (i = 0; i < n; i++)
-   		tmp *= M[i][p[i]];
-   	if (MergeSort(pr, 0, n) % 2 == 0)
-   		Ans += tmp;
-   	else
-   		Ans -= tmp;
-}
-
-bool NextPerm( int *p, int n )
-{
-	int i, p1 = -1, mn = -1;
-	tmp = 0;
-	for (i = n - 1; i > 0; i--)
-		if (p[i] > p[i - 1])
-		{
-			p1 = i - 1;
-			break;
-		}
-	if (p1 == -1)
-		return 0;
-	for (i = p1 + 1; i < n; i++)
-		if (p[i] > p[p1] && (mn == -1 || p[i] < p[mn]))
-			mn = i;
-   	swap(&p[p1], &p[mn]);
-   	ReverseArr(p, p1 + 1, n - 1);
-
-   	PlusDet(p, n);
-   	return 1;
-}
-
-int MergeSort( int *a, int l, int r )
-{
-	int ans = 0, m, i, j, k;
-	int b[N];
-	if (l + 1 == r)
-		return 0;
-
-	m = l + (r - l) / 2;
-	ans += MergeSort(a, l, m);
-	ans += MergeSort(a, m, r);
-
-	i = l, j = m, k = l;
-	while (i < m && j < r)
-	{
-		if (a[i] < a[j])
-			b[k++] = a[i++], ans += j - m;
-		else
-			b[k++] = a[j++];
-	}
-	while (i < m)
-		b[k++] = a[i++], ans += j - m;
-	while (j < r)
-		b[k++] = a[j++];
-	for (i = l; i < r; i++)
-		a[i] = b[i];
-	return ans;
+	double mn = m[a][b] / m[b][b];
+	for (i = 0; i < n; i++)
+		m[a][i] -= mn * m[b][i];
 }
 
 int main( void )
 {
-	int n, p[N], i, j;
-	time_t t = clock();
-	freopen("matrix.in", "rt", stdin);
+	int n, i, j, k, mxi, mxj;
+	double Ans = 1;
+	time_t t;
+	bool sgn = 1;
+	freopen(FNAME, "rt", stdin);
 	scanf("%d", &n);
+	t = clock();
 	for (i = 0; i < n; i++)
 		for (j = 0; j < n; j++)
-			scanf("%lf", &M[i][j]);
+			scanf("%lf", &m[i][j]);
+	for (i = 1; i < n; i++)
+	{
+		mxi = mxj = i;
+		for (j = i + 1; j < n; j++)
+			for (k = i + 1; k < n; k++)
+				if (m[j][k] > m[mxi][mxj] && m[j][k] != 0)
+					mxi = j, mxj = k;
+		if (mxi != i)
+			SwapH(n, mxi, i), sgn *= -1;
+		if (mxj != i)
+			SwapW(n, mxj, i), sgn *= -1;
+		for (j = i; j < n; j++)
+		{
+			Minus(n, j, i - 1);
+		}
+	}
 	for (i = 0; i < n; i++)
-		p[i] = i;
-	PlusDet(p, n);
-	while (NextPerm(p, n));
-	printf("Matrix Determination = %lf\n", Ans);
+		Ans *= m[i][i];
+	printf("Matrix Determination = %lf\n", Ans * sgn);
 	fprintf(stderr, "time: %.3f\n", (clock() - t) / (float) CLOCKS_PER_SEC);
 	return 0;
 }
